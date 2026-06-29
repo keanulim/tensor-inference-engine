f4a94546ca6adc165e46127f120472f72c07f272
keanu <keanu7k@gmail.com>
2026-06-28 19:31:23 -0700

    layernorm complete, kernels done.

--- kernels.cpp
+++ kernels.cpp
@@ -3,7 +3,7 @@
 //
 #include <cmath>
 
-void gemm(const float* A, const float* B, float* C, int M, int N, int K) {
+void geMM(const float* A, const float* B, float* C, int M, int N, int K) {
     for (int i = 0; i < M; i++) {
         for (int k = 0; k < K; k++) {
             for (int j = 0; j < N; j++) {
@@ -14,13 +14,13 @@ void gemm(const float* A, const float* B, float* C, int M, int N, int K) {
     }
 }
 
-void relu(const float* in, float* out, int n) {
+void reLU(const float* in, float* out, int n) {
     for (int i = 0; i < n; i++) {
         out[i] = in[i] > 0.0f ? in[i] : 0.0f;
     }
 }
 
-void silu(const float* in, float* out, int n) {
+void siLU(const float* in, float* out, int n) {
     for ( int i = 0; i < n; i++) {
         out[i] = in[i] / (1.0f + std::exp(-in[i]));
     }
@@ -42,5 +42,35 @@ void softmax(const float* in, float* out, int n) {
     for (int i = 0; i < n; i++) {
         out[i]/=sum;
     }
+}
+
+void layerNorm(const float* in, float* out, int d, const float* weight, const float* bias,
+    float eps = 1e-5f) {
+
+    float sum = 0.0f;
+
+    for (int i = 0; i < d; i++) {
+        sum += in[i];
+    }
+
+    float mean = sum / d;
+
+    float sum_var = 0.0f;
+    for (int i = 0; i < d; i++) {
+        sum_var += (in[i] - mean) * (in[i] - mean);
+    }
+
+    float var = sum_var/d + eps;
+    float inverted_std= 1.0f /std::sqrt(var);
+
+    for (int i = 0;  i < d; i++) {
+        out[i] = (in[i] - mean) * inverted_std * weight[i] + bias[i];
+    }
+
+
+
+
+
+
 
 }
